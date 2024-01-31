3b2b72fea3c85a4e11192530adb4f2c5693a7188
AviIsAvaailable <avinash_93@hotmail.com>
2024-01-30 21:56:02 -0500

    Update PassGenerator.py

--- PassGenerator.py
+++ PassGenerator.py
@@ -1,21 +1,27 @@
 import random
 
 
-# p1 = generate12()
 def generate(num):
     result = ""
     count = 0
-    if num > 4 :
+    min_length = 4
+    if num > min_length:
         while count < num:
+            # The distribution of the password is 1/3 : lower case letters, 1/3 upper case letters
+            # and the rest as numbers
+            # Generates the lower case letters
             if count < num//3:
                 result += chr(random.randint(65, 90))
                 count += 1
+            # Generates the upper case letters
             elif count < 2*(num//3):
                 result += chr(random.randint(97, 122))
                 count += 1
+            # Generates the numbers
             else:
                 result += chr(random.randint(48, 57))
                 count += 1
+        # randomly shuffles the result
         result = ''.join(random.sample(result, len(result)))
     else:
         result = "Password must be al least 4 characters !"
